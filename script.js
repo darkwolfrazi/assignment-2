7c5240f31923e4dac8b1f060585ee61b1f815a60
Razi Montgomery <darkwolfrazi@yahoo.com>
2020-01-25 20:10:03 -0800

    finished adding code to .js file

--- script.js
+++ script.js
@@ -6,11 +6,11 @@
 console.log("hello world");
 // 2. Write code to concatenate three strings and output the result to the console:
 // "Hello", " ", "World!"
-console.log("hello" +"." +"world");
+console.log("hello" + " " +"world");
 // 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
 // *Note: You should only have one console.log() statement - how do you add a new line in
 // a string?...
-console.log("hello/n"+ "world");
+console.log("hello\n"+ "world");
 // 4. Write code to output the result of the arithmetic expression to the console:
 // 3 * 8 + 2;
 console.log(3*8+2); 
@@ -22,11 +22,13 @@ console.log(5<10);
 console.log(10>5); 
 // 7. Write code to output the result of the comparison:
 // "Apples" is NOT equal to "Oranges"
-
+console.log("apples"!="oranges");
 // 8. Write code to output the result of the comparison:
 // 10 * 10 is greater than 50 AND 5 * 5 is less than 50
-
+console.log(10 * 10 > 50 && 5 * 5 < 50);
 // 9. Write code to output the result of the comparison:
 // 1 + 2 is greater than 5 OR 1 * 10 is greater than 5
-
+console.log(1 + 2 > 5 || 1 * 10 > 5 );
 // 10. Write code that demonstrates an automatic type conversion as shown in Chapter 01.
+console.log(true ? 1 : 2);
+console.log(false ? 1 : 2);
